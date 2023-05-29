aa5ff6d95bf9bf4663e854d1136f4d9b7e660fdc
Wei Liu <liuwi@pascal-deep-01.cluster.intern>
2023-05-29 17:24:21 +0200

    batch size

--- run_task3_missing.sh
+++ run_task3_missing.sh
@@ -1,5 +1,5 @@
 ## 1.for corpora with a training set
-for dataset in eng.sdrt.stac fra.sdrt.annodis
+for dataset in eng.sdrt.stac fra.sdrt.annodis eng.pdtb.pdtb eng.rst.gum eng.rst.rstdt
 do
     python3 task3.py --do_train \
                      --dataset=${dataset}
